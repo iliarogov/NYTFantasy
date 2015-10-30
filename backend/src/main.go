030157d5f45dbb6585565d64ba41d564e0497f09
ilj <ilj>
2015-10-30 14:40:02 -0400

    serve static stuff and json

--- backend/src/main.go
+++ backend/src/main.go
@@ -3,29 +3,49 @@ package main
 import "fmt"
 import (
   "net/http"
-  "io/ioutil"
+//  "io/ioutil"
   "encoding/json"
 )
 
 func main() {
-  resp, err := http.Get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Hillary+Clinton&begin_date=20151027&end_date=20151029&api-key=0abd2e8ce8d0988d7eda50ba18f19c82:14:73341622")
-  if err != nil {
-    // handle error
-  }
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
+//  resp, err := http.Get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Hillary+Clinton&begin_date=20151027&end_date=20151029&api-key=0abd2e8ce8d0988d7eda50ba18f19c82:14:73341622")
+//  if err != nil {
+//    // handle error
+//  }
+//  defer resp.Body.Close()
+//  body, err := ioutil.ReadAll(resp.Body)
+//
+//  data := map[string]interface{}{}
+//  json.Unmarshal(body, &data);
+//
+//  response := data["response"].(map[string]interface{});
+//  docs := response["docs"].([]interface{});
+//
+//  fmt.Println(len(docs));
 
-  data := map[string]interface{}{}
-  json.Unmarshal(body, &data);
-
-  response := data["response"].(map[string]interface{});
-  docs := response["docs"].([]interface{});
-
-  fmt.Println(len(docs));
-
-  http.ListenAndServe(":8080", http.FileServer(http.Dir("./frontend/dist")))
+  http.HandleFunc("/data", foo);
+  http.HandleFunc("/", http.FileServer(http.Dir("./frontend/dist")).ServeHTTP);
+  http.ListenAndServe(":8080", nil)
 
   fmt.Println("blah");
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+  profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+
+  js, err := json.Marshal(profile)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
+}
+
+type Profile struct {
+  Name    string
+  Hobbies []string
 }
 
 
